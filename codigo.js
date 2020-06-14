8485c70dc18372b738abf363834754c66cce0026
DaniekTj <65110807+daniektj@users.noreply.github.com>
2020-06-14 06:10:50 -0700

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,9 +1,4 @@
-
-let vh = window.innerHeight * 0.01;
-
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+window.addEventListener('resize', () => { 
+  document.querySelector(':root').style
+    .setProperty('--vh', window.innerHeight/100 + 'px');
+})
