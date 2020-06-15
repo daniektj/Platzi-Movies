6248dbd99e89ea3803f070dfb3bc1a91faf20391
DaniekTj <65110807+daniektj@users.noreply.github.com>
2020-06-15 04:23:58 -0700

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,4 +1,4 @@
-window.addEventListener('resize', () => { 
-  document.querySelector(':root').style
-    .setProperty('--vh', window.innerHeight/100 + 'px');
-})
+//window.addEventListener('resize', () => { 
+ // document.querySelector(':root').style
+   // .setProperty('--vh', window.innerHeight/100 + 'px');
+//})
